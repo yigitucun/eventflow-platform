5d415684280cb087a2770bd2a1e63270bc7b4824
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2025-11-14 19:33:17 +0300

    Adds security configuration
    Configures Spring Security to handle user authentication.
    This includes disabling CSRF protection, permitting all requests,
    and setting up a BCryptPasswordEncoder.

---
+++ user-service/src/main/java/io/yigitucun/eventflow/user/service/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package io.yigitucun.eventflow.user.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests(request->request
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
