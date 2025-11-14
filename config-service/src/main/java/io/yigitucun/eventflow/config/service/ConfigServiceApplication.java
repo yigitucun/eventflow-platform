362b7f694951ae3dbe405795c83ab18a237fb3e3
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2025-11-14 15:17:43 +0300

    update config-service

--- config-service/src/main/java/io/yigitucun/eventflow/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/io/yigitucun/eventflow/config/service/ConfigServiceApplication.java
@@ -1,9 +1,11 @@
-package io.yigitucun.eventflow.config_service;
+package io.yigitucun.eventflow.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
--- config-service/src/test/java/io/yigitucun/eventflow/config/service/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/io/yigitucun/eventflow/config/service/ConfigServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package io.yigitucun.eventflow.config_service;
+package io.yigitucun.eventflow.config.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
