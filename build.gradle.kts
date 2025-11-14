ea0a29f674a21ab8d4056a8136fcc72915d48a8a
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2025-11-14 15:21:43 +0300

    add build.gradle

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.springframework.boot") version "3.5.7" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+    id("java")
+}
+
+allprojects {
+    group = "io.yigitucun.eventflow"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "java")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+}
