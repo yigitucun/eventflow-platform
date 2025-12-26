4d2e6a2307c641dde4fe508b7ce7baa9dd3cf6dd
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2025-12-26 23:39:08 +0300

    feat: Migrate auth-service to mvc-lib

--- auth-service/src/main/java/io/yigitucun/eventflow/auth/service/AuthServiceApplication.java
+++ auth-service/src/main/java/io/yigitucun/eventflow/auth/service/AuthServiceApplication.java
@@ -7,11 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
-@ComponentScan(basePackages = {
-		"io.yigitucun.eventflow.auth.service",
-		"io.yigitucun.eventflow.exceptions",
-		"io.yigitucun.eventflow.advice",
-})
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
